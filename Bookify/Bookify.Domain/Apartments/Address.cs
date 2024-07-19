c54232934985c01eac2c649427e1dff2a0b25dea
nieshuo <694891769@qq.com>
2024-07-19 15:59:13 +0800

    1丶提交Apartments的Entity

---
+++ Bookify/Bookify.Domain/Apartments/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace Bookify.Domain.Apartments
+{
+    /// <summary>
+    /// 地址值对象
+    /// </summary>
+    /// <param name="Country"></param>
+    /// <param name="State"></param>
+    /// <param name="ZipCode"></param>
+    /// <param name="City"></param>
+    /// <param name="Street"></param>
+    public record Address(
+        string Country,
+        string State,
+        string ZipCode,
+        string City,
+        string Street
+    );
+}
---
+++ Bookify/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,30 @@
+﻿namespace Bookify.Domain.Apartments
+{
+    public record Currency
+    {
+        internal static readonly Currency None = new("");
+        public static readonly Currency Usd = new Currency("USD");
+        public static readonly Currency Eur = new Currency("EUR");
+        public static readonly Currency Cny = new Currency("CNY");
+
+        public string Code { get; init; }
+
+        private Currency(string code)
+        {
+            Code = code;
+        }
+
+        public static Currency FromCode(string code)
+        {
+            return All.FirstOrDefault(x => x.Code == code)
+                ?? throw new ApplicationException("The currency code is invalid");
+        }
+
+        public static readonly IReadOnlyCollection<Currency> All = new List<Currency>
+        { 
+            Usd,
+            Eur,
+            Cny,
+        };
+    }
+}
